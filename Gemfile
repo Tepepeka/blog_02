4805a3a881afee5d1a4ec9e0f24ece897f5186e9
Tepepeka <jeremy.bourgoin@live.fr>
2023-02-13 21:37:26 +0100

    Part 1 . monday

--- Gemfile
+++ Gemfile
@@ -3,6 +3,11 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.2.0"
 
+# My gems
+gem "pry"
+gem "table_print"
+gem "faker"
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.4", ">= 7.0.4.2"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+# destroy_all
+Article.destroy_all
+User.destroy_all
+City.destroy_all
+
+
+# Cities create
+10.times do |i|
+    City.create(
+        name:Faker::Address.city,
+        zip_code:Faker::Address.zip_code
+    )
+    puts "*"*(i+1)
+    puts "#{i} city(ies) created"
+end
+
+
+# Users create
+10.times do |i|
+    name = Faker::Name.name
+    User.create(
+        name:name,
+        email:"#{name}@love.fr",
+        age:rand(20..40),
+        city_id:City.all.sample.id
+    )
+    puts "*"*(i+1)
+    puts "#{i} user(s) created"
+end
+
+
+# Articles create
+20.times do |i|
+    Article.create(
+        title:Faker::Book.title,
+        body:Faker::Lorem.paragraphs(number: 2),
+        user_id:User.all.sample.id
+    )
+    puts "*"*(i+1)
+    puts "#{i} article(s) created"
+end
