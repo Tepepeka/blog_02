8feefb2bc4566bab8463041ae583c74e67e657d5
Tepepeka <jeremy.bourgoin@live.fr>
2023-02-15 02:24:46 +0100

    add Crud / alert / some fixes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,21 +1,54 @@
 class ArticlesController < ApplicationController
 
-    before_action :set_article, only: [:show]
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
-    def index
-        @articles = Article.all
+  def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.user_id = 12 #car on est pas connecté donc pas de current_user id
+    if @article.save
+      redirect_to @article, notice: "Article was successfully created."
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
 
-    def show
+  def edit
+  end
 
+  def update
+    if @article.update(article_params)
+      redirect_to @article, notice: "Article was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
     end
+  end
 
-    #######
-    private
-    #######
+  def destroy
+    @article.destroy
+    redirect_to root_path, status: :see_other, notice: "Article was successfully destroyed."
+  end
 
-    def set_article
-        @article=Article.find(params[:id])
-    end
+  #######
+  private
+  #######
+
+  def set_article
+    @article=Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :body, :user_id)
+  end
 
 end
