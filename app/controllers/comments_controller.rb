05ca17a0fc8868e7675c10a79c3eaaeb896d81c0
Tepepeka <jeremy.bourgoin@live.fr>
2023-02-16 13:16:36 +0100

    delete comment ok, resize div

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,32 +1,32 @@
 class CommentsController < ApplicationController
 
-    def create
-      @article = Article.find(params[:article_id])
-     
-      @comment = @article.comments.new(comment_params)
-      @comment.user_id = 33  #car on est pas connecté donc pas de current_user id (@comment.user = current_user)
-      if @comment.save
-        redirect_to article_path(@article), notice: "Comment was successfully created."
-      else
-        redirect_to article_path(@article), alert: "Comment was not created."
-      end
+  def create
+    @article = Article.find(params[:article_id])
 
+    @comment = @article.comments.new(comment_params)
+    @comment.user_id = 33  #car on est pas connecté donc pas de current_user id (@comment.user = current_user)
+    if @comment.save
+      redirect_to article_path(@article), notice: "Comment was successfully created."
+    else
+      redirect_to article_path(@article), alert: "Comment was not created."
     end
 
-#penser a faire un destroy, peut etre un edit/update aussi
-def destroy
-  @article = Article.find(params[:article_id])
-  @comment = @article.comments.find(params[:id])
-  @comment.destroy
-  redirect_to article_path(@article), alert: "Comment was successfully destroyed."
-end
+  end
 
-    #######
-    private
-    #######
 
-    def comment_params
-        params.require(:comment).permit(:body)
-    end
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article), status: :see_other, notice: "Comment was successfully destroyed."
+  end
+
+  #######
+  private
+  #######
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 
 end
